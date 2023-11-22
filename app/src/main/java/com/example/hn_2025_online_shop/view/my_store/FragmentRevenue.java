3e04060f35ec37e387902d6d896787f447d87acc
MinhNhat <fatdinasours@gmail.com>
2023-11-22 09:56:04 +0700

    nmn update 22/11

--- app/src/main/java/com/example/hn_2025_online_shop/view/my_store/FragmentRevenue.java
+++ app/src/main/java/com/example/hn_2025_online_shop/view/my_store/FragmentRevenue.java
@@ -1,37 +1,21 @@
 package com.example.hn_2025_online_shop.view.my_store;
-
 import android.graphics.Color;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.example.hn_2025_online_shop.R;
 import com.example.hn_2025_online_shop.databinding.FragmentRevenueBinding;
-import com.github.mikephil.charting.charts.CombinedChart;
-import com.github.mikephil.charting.charts.HorizontalBarChart;
-import com.github.mikephil.charting.components.AxisBase;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.CombinedData;
-import com.github.mikephil.charting.data.DataSet;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.formatter.IAxisValueFormatter;
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
-import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
-
 import java.util.ArrayList;
-import java.util.List;
 
 public class FragmentRevenue extends Fragment {
     private FragmentRevenueBinding binding;
@@ -47,17 +31,15 @@ public class FragmentRevenue extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding= FragmentRevenueBinding.inflate(getLayoutInflater());
         binding.combinedChart.getDescription().setText("Combined Chart Example");
-
         // Thiết lập độ dời ngang để cho phép cuộn ngang
-//        binding.combinedChart.setViewPortOffsets(100, 0, 100, 0);
         binding.combinedChart.setDrawBarShadow(false);
         binding.combinedChart.setHorizontalScrollBarEnabled(true);
         binding.combinedChart.setData(generateCombinedData());
-        binding.combinedChart.setScrollBarSize(10);
+        binding.combinedChart.setScrollBarSize(12);
         return binding.getRoot();
     }
 
@@ -85,7 +67,6 @@ public class FragmentRevenue extends Fragment {
     private LineData generateLineData() {
         // Tạo danh sách các điểm dữ liệu cho LineChart
         ArrayList<Entry> entries = new ArrayList<>();
-        entries.add(new Entry(0, 4));
         entries.add(new Entry(1, 8));
         entries.add(new Entry(2, 6));
         entries.add(new Entry(3, 2));
@@ -93,10 +74,10 @@ public class FragmentRevenue extends Fragment {
         entries.add(new Entry(5, 3));
         entries.add(new Entry(6, 1));
         entries.add(new Entry(8, 3));
-        entries.add(new Entry(9, 3));
-        entries.add(new Entry(10, 3));
-        entries.add(new Entry(11, 3));
-        entries.add(new Entry(12, 3));
+        entries.add(new Entry(9, 5));
+        entries.add(new Entry(10,3));
+        entries.add(new Entry(11,2));
+        entries.add(new Entry(12,4));
 
         // Tạo LineDataSet từ danh sách các điểm dữ liệu
         LineDataSet lineDataSet = new LineDataSet(entries, "Thu nhập");
@@ -114,7 +95,6 @@ public class FragmentRevenue extends Fragment {
     private BarData generateBarData() {
         // Tạo danh sách các điểm dữ liệu cho BarChart
         ArrayList<BarEntry> entries = new ArrayList<>();
-        entries.add(new BarEntry(0, 5));
         entries.add(new BarEntry(1, 2));
         entries.add(new BarEntry(2, 4));
         entries.add(new BarEntry(3, 6));
@@ -122,11 +102,11 @@ public class FragmentRevenue extends Fragment {
         entries.add(new BarEntry(5, 4));
         entries.add(new BarEntry(6, 1));
         entries.add(new BarEntry(7, 3));
-        entries.add(new BarEntry(8, 3));
-        entries.add(new BarEntry(9, 3));
-        entries.add(new BarEntry(10, 3));
-        entries.add(new BarEntry(11, 3));
-        entries.add(new BarEntry(12, 3));
+        entries.add(new BarEntry(8, 1));
+        entries.add(new BarEntry(9, 2));
+        entries.add(new BarEntry(10,3));
+        entries.add(new BarEntry(11,5));
+        entries.add(new BarEntry(12,7));
 
         // Tạo BarDataSet từ danh sách các điểm dữ liệu
         BarDataSet barDataSet = new BarDataSet(entries, "Chi phí");
